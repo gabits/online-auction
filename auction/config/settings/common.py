4ba0af6e94ec29c9960271996a0f05ad87d8b6bd
Gabriela Valle <gabriela@spirable.com>
2020-04-13 03:38:11 +0100

    Do not commit the database anymore

--- auction/config/settings/common.py
+++ auction/config/settings/common.py
@@ -51,10 +51,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #
 #   https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 #
+# Set MySQL as the default database service, pointing to an external host
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': '5432',
     }
 }
 
