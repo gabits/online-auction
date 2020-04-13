9b6b43c54fdc1b43e8da02dfe546629a6191d8dd
Gabriela Valle <gabriela@spirable.com>
2020-04-13 03:21:01 +0100

    Split settings into multiple files to reflect differences across different environments; change config path for WSGI and ASGI set ups

---
+++ auction/config/settings/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from .common import *
+
+# Determine current environment
+APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT", "dev")
+
+
+# Import settings according to current environment.
+#
+# Following the 12-factor app recommendations (https://12factor.net/),
+# these two should have the minimum amount of discrepancies among themselves.
+if APP_ENVIRONMENT == "dev":
+    from .development import *
+elif APP_ENVIRONMENT == "prod":
+    from .production import *
