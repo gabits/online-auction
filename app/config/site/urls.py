9fcca6704b30f6dc5cf05711a5e78834509cc31c
Gabriela Valle <gabriela@spirable.com>
2020-04-19 18:46:55 +0100

    Redirect all requests to homepage to Admin site

--- app/config/site/urls.py
+++ app/config/site/urls.py
@@ -1,8 +1,10 @@
 # Django
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 # Local
+from django.views.generic import RedirectView
+
 from config.api import urls as api_urls
 
 app_name = 'site'
@@ -11,4 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urls, namespace='api')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # Redirect all URLs to the admin site.
+    re_path('^$', RedirectView.as_view(url='admin'), name='home'),
 ]
